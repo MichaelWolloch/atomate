10d94786a9308dd4f051a68c7578e2f6ef87adef
mwolloch <michael.wolloch@univie.ac.at>
2021-09-22 15:56:55 +0200

    added support to copy more files from the previous calculation in StaticFW.

--- atomate/vasp/fireworks/core.py
+++ atomate/vasp/fireworks/core.py
@@ -308,6 +308,7 @@ class StaticFW(Firework):
         vasptodb_kwargs=None,
         parents=None,
         spec_structure_key=None,
+        additional_files_from_prev_calc=[],
         **kwargs,
     ):
         """
@@ -355,12 +356,15 @@ class StaticFW(Firework):
                 )
             )
         elif prev_calc_dir:
-            t.append(CopyVaspOutputs(calc_dir=prev_calc_dir, contcar_to_poscar=True))
+            t.append(
+                CopyVaspOutputs(calc_dir=prev_calc_dir, contcar_to_poscar=True,
+                                additional_files=additional_files_from_prev_calc))
             t.append(WriteVaspStaticFromPrev(other_params=vasp_input_set_params))
         elif parents:
             if prev_calc_loc:
                 t.append(
-                    CopyVaspOutputs(calc_loc=prev_calc_loc, contcar_to_poscar=True)
+                    CopyVaspOutputs(calc_loc=prev_calc_loc, contcar_to_poscar=True,
+                                    additional_files=additional_files_from_prev_calc)
                 )
             t.append(WriteVaspStaticFromPrev(other_params=vasp_input_set_params))
         elif structure:
